4462a399c4c8670e1f9b05cfcc6493b06789ebf1
haruki7049 <tontonkirikiri@gmail.com>
2026-05-23 15:03:28 +0900

    Update

--- src/do-compute-computers.zig
+++ src/do-compute-computers.zig
@@ -11,139 +11,31 @@ pub fn gen(allocator: std.mem.Allocator) !lightmix.Wave(f64) {
     const channels = 1;
     const volume: f64 = 0.25;
 
-    var _4_4_c4_sawtooth = try Sawtooth.gen(
+    var _4_4_c3_sawtooth = try Sawtooth.gen(
         f64,
         allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .c, .octave = 4 }),
+        sample_rate / 9,
+        Scale.gen(.{ .code = .c, .octave = 3 }),
         sample_rate,
         channels,
         volume,
     );
-    try _4_4_c4_sawtooth.filter(Filters.decay);
-    defer _4_4_c4_sawtooth.deinit();
-
-    var _4_4_e4_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .e, .octave = 4 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_e4_sawtooth.filter(Filters.decay);
-    defer _4_4_e4_sawtooth.deinit();
-
-    var _4_4_g4_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .g, .octave = 4 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_g4_sawtooth.filter(Filters.decay);
-    defer _4_4_g4_sawtooth.deinit();
-
-    var _4_4_gs4_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .gs, .octave = 4 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_gs4_sawtooth.filter(Filters.decay);
-    defer _4_4_gs4_sawtooth.deinit();
-
-    var _4_4_c5_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .c, .octave = 5 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_c5_sawtooth.filter(Filters.decay);
-    defer _4_4_c5_sawtooth.deinit();
-
-    var _4_4_ds5_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .ds, .octave = 5 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_ds5_sawtooth.filter(Filters.decay);
-    defer _4_4_ds5_sawtooth.deinit();
-
-    var _4_4_as5_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .as, .octave = 5 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_as5_sawtooth.filter(Filters.decay);
-    defer _4_4_as5_sawtooth.deinit();
-
-    var _4_4_d6_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .d, .octave = 6 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_d6_sawtooth.filter(Filters.decay);
-    defer _4_4_d6_sawtooth.deinit();
-
-    var _4_4_f6_sawtooth = try Sawtooth.gen(
-        f64,
-        allocator,
-        44100 / 9,
-        Scale.gen(.{ .code = .f, .octave = 6 }),
-        sample_rate,
-        channels,
-        volume,
-    );
-    try _4_4_f6_sawtooth.filter(Filters.decay);
-    defer _4_4_f6_sawtooth.deinit();
+    try _4_4_c3_sawtooth.filter(Filters.decay);
+    defer _4_4_c3_sawtooth.deinit();
 
     return try Splitter.gen(
         f64,
         allocator,
-        30000,
+        sample_rate * 4,
         &.{
-            _4_4_c4_sawtooth,
-            _4_4_e4_sawtooth,
-            _4_4_g4_sawtooth,
-            _4_4_c4_sawtooth,
-            _4_4_e4_sawtooth,
-            _4_4_g4_sawtooth,
-
-            _4_4_gs4_sawtooth,
-            _4_4_c5_sawtooth,
-            _4_4_ds5_sawtooth,
-            _4_4_gs4_sawtooth,
-            _4_4_c5_sawtooth,
-            _4_4_ds5_sawtooth,
-
-            _4_4_as5_sawtooth,
-            _4_4_d6_sawtooth,
-            _4_4_f6_sawtooth,
-            _4_4_as5_sawtooth,
-            _4_4_d6_sawtooth,
-            _4_4_f6_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
+            _4_4_c3_sawtooth,
         },
         sample_rate,
         channels,
